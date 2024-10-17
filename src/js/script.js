7665115b291b73bb8ea7b64baa40c134f2515a9c
Élcio Reis <elcio.reis024@gmail.com>
2024-10-17 10:48:52 -0300

    Adicionado link ao button

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,3 @@
-/* document.querySelector('.contato').addEventListener('click', (event) => {
-    window.open('');
-}) */
+document.querySelector('.contato').addEventListener('click', (event) => {
+    window.open('https://api.whatsapp.com/send/?phone=5599999531016&text&type=phone_number&app_absent=0');
+})

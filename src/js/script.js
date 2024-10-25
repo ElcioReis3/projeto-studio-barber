2cd939aa36da300c4626df295fd9fa86160d5193
Élcio Reis <elcio.reis024@gmail.com>
2024-10-25 11:31:44 -0300

    Adicionado mensagem ao button de contato

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 document.querySelector('.contato').addEventListener('click', (event) => {
-    window.open('https://api.whatsapp.com/send/?phone=5599999531016&text&type=phone_number&app_absent=0');
+    const numero = 5599999531016;
+    const msg = 'Olá, gostaria de saber como está a agenda da barbearia, gostaria de marcar um horário.';
+    const url = `https://api.whatsapp.com/send?phone=${numero}&text=${encodeURIComponent(msg)}`;
+
+    window.open(url, '_blank');
 })
